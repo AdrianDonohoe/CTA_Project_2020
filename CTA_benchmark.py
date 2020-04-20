e4b46642dd37ee864a164971c794fb71a7b88765
Adrian Donohoe <allezlesblues1@gmail.com>
2020-04-20 11:05:55 +0100

    all 5 algorithms added and copy of same array used for each n

--- CTA_benchmark.py
+++ CTA_benchmark.py
@@ -18,7 +18,8 @@ indexes = ['bubble', 'merge', 'counting', 'insertion','selection']
 times_df =  pd.DataFrame(index=indexes,columns=cols)
 times_df
 
-array_size = [100, 250, 500, 750, 1000, 1250, 2500, 3750, 5000, 6250, 7500, 8750, 10000]
+#array_size = [100, 250, 500, 750, 1000, 1250, 2500, 3750, 5000, 6250, 7500, 8750, 10000]
+array_size = [100,250,500,750,1000,1250] # for testing
 
 ##################   BubbleSort##################
 # adapted from https://runestone.academy/runestone/books/published/pythonds/SortSearch/TheBubbleSort.html
@@ -63,49 +64,132 @@ def mergeSort(alist):
             k=k+1
     #print("Merging ",alist)
 
+# Adapted from https://www.w3resource.com/python-exercises/data-structures-and-algorithms/python-search-and-sorting-exercise-10.php
+def countingSort(array1):
+    m = len(array1) 
+    count = [0] * m   # make an array with m elements containing 0             
+    
+    for a in array1: # itareate over the array to be sorted
+    # count occurences of a and increment the count array element for that a
+        count[a] += 1             
+    i = 0
+    for a in range(m):    # iterate over values of the array to be counted        
+        for c in range(count[a]):  # fill the array to be counted with count[a] times value a 
+            array1[i] = a
+            i += 1
+    return array1
 
+# Adapted from https://runestone.academy/runestone/books/published/pythonds/SortSearch/TheInsertionSort.html
+def insertionSort(alist):
+   for index in range(1,len(alist)): # Assume 1st element is sorted, so iterate from 1 to end of array
+
+     currentvalue = alist[index]
+     position = index
+
+     while position>0 and alist[position-1]>currentvalue: # This loop will compare  current position against all previous elements and swap them if current position is less than previous element, until postion is 0.
+         alist[position]=alist[position-1] # swap with previous element if loop condition met
+         position = position-1 # decrement position , so in next loop it will now be compared with previous element
+
+     alist[position]=currentvalue # Second part of the swap (if swapping) or just assigning the same value if not swapping
+
+# Adapted from https://runestone.academy/runestone/books/published/pythonds/SortSearch/TheSelectionSort.html
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
 
 def main():
-    ### Call BubbleSort
     for i in range(0,len(array_size)):
-        total_time = 0
+        total_time_bubble = 0
+        total_time_merge = 0
+        total_time_counting = 0
+        total_time_insertion = 0
+        total_time_selection = 0
         print("array size: " + str(array_size[i]))
         for j in range(10):
-            my_array = random_array(array_size[i])
-            start_time = time.time()
-            bubbleSort(my_array)
-            finish_time = time.time()
-            time_elapsed = finish_time - start_time
-            print('Run number ' + str(j) + ' ', time_elapsed)
-            total_time += time_elapsed
-        print('Total time: ', total_time)
+            my_array_bubble = random_array(array_size[i]) 
+            my_array_merge = list(my_array_bubble) # Adapted from https://stackoverflow.com/a/2612815
+            my_array_counting = list(my_array_bubble)
+            my_array_insertion = list(my_array_bubble)
+            my_array_selection = list(my_array_bubble)
+
+            # do the Bubble Sort
+            print(my_array_bubble)
+            start_time_bubble = time.time()
+            bubbleSort(my_array_bubble)
+            finish_time_bubble = time.time()
+            print(my_array_bubble)
+            time_elapsed_bubble = finish_time_bubble - start_time_bubble
+            print('Run number ' + str(j) + ' ', time_elapsed_bubble)
+            total_time_bubble += time_elapsed_bubble
+
+            # do the Merge Sort
+            print(my_array_merge)
+            start_time_merge = time.time()
+            mergeSort(my_array_merge)
+            finish_time_merge = time.time()
+            print(my_array_merge)
+            time_elapsed_merge = finish_time_merge - start_time_merge
+            print('Run number ' + str(j) + ' ', time_elapsed_merge)
+            total_time_merge += time_elapsed_merge
+
+            # do the Counting Sort
+            print(my_array_counting)
+            start_time_counting = time.time()
+            countingSort(my_array_counting)
+            finish_time_counting = time.time()
+            print(my_array_counting)
+            time_elapsed_counting = finish_time_counting - start_time_counting
+            print('Run number ' + str(j) + ' ', time_elapsed_counting)
+            total_time_counting += time_elapsed_counting
+
+            # do the Insertion Sort
+            print(my_array_insertion)
+            start_time_insertion = time.time()
+            insertionSort(my_array_insertion)
+            finish_time_insertion = time.time()
+            print(my_array_insertion)
+            time_elapsed_insertion = finish_time_insertion - start_time_insertion
+            print('Run number ' + str(j) + ' ', time_elapsed_insertion)
+            total_time_insertion += time_elapsed_insertion
+
+            # do the Selection Sort
+            print(my_array_selection)
+            start_time_selection = time.time()
+            selectionSort(my_array_selection)
+            finish_time_selection = time.time()
+            print(my_array_selection)
+            time_elapsed_selection = finish_time_selection - start_time_selection
+            print('Run number ' + str(j) + ' ', time_elapsed_selection)
+            total_time_selection += time_elapsed_selection
+
+
+        print('Bubble Sort Total time: ', total_time_bubble)
+        print('Merge Sort Total time: ', total_time_merge)
+        print('Counting Sort Total time: ', total_time_counting)
+        print('Insertion Sort Total time: ', total_time_insertion)
+        print('Selection Sort Total time: ', total_time_selection)
     
-        times_df.iat[0,i] = round((total_time/10),3)
-        print('Avg: ', times_df.iat[0,i])
+        times_df.iat[0,i] = round((total_time_bubble/10),3)
+        times_df.iat[1,i] = round((total_time_merge/10),3)
+        times_df.iat[2,i] = round((total_time_counting/10),3)
+        times_df.iat[3,i] = round((total_time_insertion/10),3)
+        times_df.iat[4,i] = round((total_time_selection/10),3)
+
+        print('Bubble Sort Avg: ', times_df.iat[0,i])
+        print('Merge Sort Avg: ', times_df.iat[1,i])
+        print('Counting Sort Avg: ', times_df.iat[2,i])
+        print('Insertion Sort Avg: ', times_df.iat[3,i])
+        print('Selection Sort Avg: ', times_df.iat[4,i])
+
     with pd.option_context('display.max_rows', None, 'display.max_columns', None): # Pretty print of Dataframe. Adapted from https://stackoverflow.com/a/30691921
         print(times_df)
-
-    ############ Call MergeSort
-    for i in range(0,len(array_size)):
-        total_time = 0
-        print("array size: " + str(array_size[i]))
-        for j in range(10):
-            my_array = random_array(array_size[i])
-            start_time = time.time()
-            mergeSort(my_array)
-            finish_time = time.time()
-            time_elapsed = finish_time - start_time
-            print('Run number ' + str(j) + ' ', time_elapsed)
-            total_time += time_elapsed
-        #print(my_array)
-        print('Total time: ', total_time)
-    
-        times_df.iat[1,i] = round((total_time/10),3)
-        print('Avg: ', times_df.iat[1,i])
-
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(times_df)
-    
 
 
 if __name__ == "__main__":
